60b87b913e863ac51c48c621173169136db21800
hiteshahire98 <hituahire1998@gmail.com>
2022-10-06 15:15:56 +0530

    c

--- Constructors/Department.cs
+++ Constructors/Department.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace Constructors
 {
-    internal class Department
+    public class Department
     {
+        private int deptid;
+        private string deptname;
+        private string location;
 
+        public int Deptid
+        { 
+          get { return deptid; }
+          set { deptid = value; }
+        }
+
+        public string Deptname
+        {
+            get { return deptname; }
+            set { deptname = value; }
+        }
+
+        public int Location
+        {
+            get { return Location; }
+            set { Location = value; }
+        }
+
+        public override string ToString()
+        { 
+           return $"Department Id:{deptid} Name:{deptname} Location:{location}";
+        }
     }
 }
